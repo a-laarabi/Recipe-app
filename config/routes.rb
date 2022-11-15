3bdbb9dd1dac32cff7147d0267e217f5616b4f3d
Anasse LAARABI <contact.laarabi@gmail.com>
2022-11-15 23:03:01 +0100

    Update gitignore && routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users
+ 
+  root 'public_recipes#index'
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit] 
+    resources :shopping_lists, only: [:index, :show]    
+  end
+  resources :foods
+  resources :public_recipes, only: [:index]
 end
