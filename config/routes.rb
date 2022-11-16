355f1eddc827e87b2962b7a9d9e5e91ab9c35d0e
Tarek <83262890+tarek102@users.noreply.github.com>
2022-11-16 13:28:15 +0200

    login and registration built with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'public_recipes#index'
+  resources :recipes, only: [:index, :show, :new, :create, :destroy] do
+    resources :recipe_foods, only: [:new, :create, :destroy, :update, :edit] 
+    resources :shopping_lists, only: [:index, :show]    
+  end
+  resources :foods
+  resources :public_recipes, only: [:index]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do  
+    get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
 end
