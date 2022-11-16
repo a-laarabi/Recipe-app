524ba349bedaeecaf1e8b619ffd5ba21b7f97278
Tarek <83262890+tarek102@users.noreply.github.com>
2022-11-16 18:59:29 +0200

    add role to schema

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,5 @@
 class RecipesController < ApplicationController
-  load_and_authorize_resource
+  # load_and_authorize_resource
   before_action :set_recipe, only: %i[show edit update destroy]
 
   def index
